69a3fbd82c141b725218f25d1a50bd66c57977ef
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-03-18 00:18:35 +0700

    feat: write data to clickhouse func

---
+++ src/streaming/sink.py
@@ -0,0 +1,16 @@
+from configs.settings import ClickHouseConfig
+
+def write_to_clickhouse(df, batch_id):
+    if df.count() > 0:
+        print(f"Writing {df.count()} rows to ClickHouse (Batch {batch_id})...")
+        df.write \
+            .format("jdbc") \
+            .option("url", "jdbc:clickhouse://localhost:8123/default?jdbcCompliant=false")  \
+            .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
+            .option("dbtable", "orders_warehouse") \
+            .option("user", "default") \
+            .option("password", "1111") \
+            .mode("append") \
+            .save()
+    else:
+        print(f"Batch {batch_id} is empty. Skipping...")
