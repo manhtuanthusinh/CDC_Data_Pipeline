6718421b089577b3906badf6699d2a8a89afcf7d
Tuan Lai <tuanlaimanh20041@gmail.com>
2026-03-26 19:39:09 +0700

    fix: add time count for writing to clickhouse log

--- src/streaming/sink.py
+++ src/streaming/sink.py
@@ -1,6 +1,11 @@
 from configs.settings import ClickHouseConfig
+import time
 
 def write_to_clickhouse(df, batch_id):
+
+    start = time.time()
+    count = df.count()
+
     if df.count() > 0:
         print(f"Writing {df.count()} rows to ClickHouse (Batch {batch_id})...")
         df.write \
@@ -12,5 +17,9 @@ def write_to_clickhouse(df, batch_id):
             .option("password", "1111") \
             .mode("append") \
             .save()
+        
+        duration = time.time() - start
+        print(f"[Batch {batch_id}] Finished writing {count} rows in {duration:.2f}s")
+
     else:
         print(f"Batch {batch_id} is empty. Skipping...")
